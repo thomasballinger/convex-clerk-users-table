5aa84cdd05aedf61fa12123a328976738e9c7fe9
Tom Ballinger <tom@convex.dev>
2023-07-26 11:01:24 -0700

    Use UserJSON type

--- convex/listMessages.ts
+++ convex/listMessages.ts
@@ -1,13 +1,14 @@
 import { UserJSON } from "@clerk/backend";
 import { query } from "./_generated/server";
+import { userById } from "./users";
 
-export default query(async ({ db }) => {
-  const messages = await db.query("messages").collect();
+export default query(async (ctx) => {
+  const messages = await ctx.db.query("messages").collect();
   return Promise.all(
     messages.map(async (message) => {
       // For each message in this channel, fetch the `User` who wrote it and
       // insert their name into the `author` field.
-      const user = await db.get(message.user);
+      const user = await userById(ctx, message.user);
       return {
         author: user ? `${user.clerkUser.first_name} ${user.clerkUser.last_name}` : "deleted user",
         color: user ? user.color : "black",
--- convex/users.ts
+++ convex/users.ts
@@ -7,7 +7,7 @@ import {
 } from "./_generated/server";
 
 import { v } from "convex/values";
-import { Doc } from "./_generated/dataModel";
+import { Doc, Id } from "./_generated/dataModel";
 import { UserJSON } from "@clerk/backend";
 
 /**
@@ -39,28 +39,22 @@ export const userLoginStatus = query(
   }
 );
 
-/** The current user, containing user preferences and Clerk user info */
+/** The current user, containing user preferences and Clerk user info. */
 export const currentUser = query((ctx: QueryCtx) => getCurrentUser(ctx));
 
 /** Get user by Clerk use id (AKA "subject" on auth)  */
 export const getUser = internalQuery({
   args: { subject: v.string() },
   async handler(ctx, args): Promise<Doc<"users"> | null> {
-    return await ctx.db
-      .query("users")
-      .withIndex("by_clerk_id", (q) => q.eq("clerkUser.id", args.subject))
-      .unique();
+    return await userQuery(ctx, args.subject);
   },
 });
 
-/** Update */
+/** Create a new Clerk user or update existing Clerk user data. */
 export const updateOrCreateUser = internalMutation({
-  args: { clerkUser: v.any() },
+  args: { clerkUser: v.any() }, // no runtime validation, trust Clerk
   async handler(ctx, { clerkUser }: { clerkUser: UserJSON }) {
-    const userRecord = await ctx.db
-      .query("users")
-      .withIndex("by_clerk_id", (q) => q.eq("clerkUser.id", clerkUser.id))
-      .unique();
+    const userRecord = await userQuery(ctx, clerkUser.id);
 
     if (userRecord === null) {
       const colors = ["red", "green", "blue"];
@@ -72,13 +66,11 @@ export const updateOrCreateUser = internalMutation({
   },
 });
 
+/** Delete a user by clerk user ID. */
 export const deleteUser = internalMutation({
   args: { id: v.string() },
   async handler(ctx, { id }) {
-    const userRecord = await ctx.db
-      .query("users")
-      .withIndex("by_clerk_id", (q) => q.eq("clerkUser.id", id))
-      .unique();
+    const userRecord = await userQuery(ctx, id);
 
     if (userRecord === null) {
       console.warn("can't delete user, does not exist", id);
@@ -88,6 +80,7 @@ export const deleteUser = internalMutation({
   },
 });
 
+/** Set the user preference of the color of their text. */
 export const setColor = mutation({
   args: { color: v.string() },
   handler: async (ctx, { color }) => {
@@ -98,15 +91,29 @@ export const setColor = mutation({
 
 // Helpers
 
+export async function userQuery(
+  ctx: QueryCtx,
+  clerkUserId: string
+): Promise<(Omit<Doc<"users">, "clerkUser"> & { clerkUser: UserJSON }) | null> {
+  return await ctx.db
+    .query("users")
+    .withIndex("by_clerk_id", (q) => q.eq("clerkUser.id", clerkUserId))
+    .unique();
+}
+
+export async function userById(
+  ctx: QueryCtx,
+  id: Id<"users">
+): Promise<(Omit<Doc<"users">, "clerkUser"> & { clerkUser: UserJSON }) | null> {
+  return await ctx.db.get(id);
+}
+
 async function getCurrentUser(ctx: QueryCtx): Promise<Doc<"users"> | null> {
   const identity = await ctx.auth.getUserIdentity();
   if (identity === null) {
     return null;
   }
-  return await ctx.db
-    .query("users")
-    .withIndex("by_clerk_id", (q) => q.eq("clerkUser.id", identity.subject))
-    .unique();
+  return await userQuery(ctx, identity.subject);
 }
 
 export async function mustGetCurrentUser(ctx: QueryCtx): Promise<Doc<"users">> {
