011d79c4bf2729df99f2fd3fad69581af5ce0ac3
Tom Ballinger <tom@convex.dev>
2024-11-15 13:38:50 -0800

    new instance and new clerk account

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -5,7 +5,7 @@ export default {
       // or with `process.env.CLERK_JWT_ISSUER_DOMAIN`
       // and configure CLERK_JWT_ISSUER_DOMAIN on the Convex Dashboard
       // See https://docs.convex.dev/auth/clerk#configuring-dev-and-prod-instances
-      domain: "https://flying-lemur-2.clerk.accounts.dev",
+      domain: "https://bursting-cicada-32.clerk.accounts.dev",
       applicationID: "convex",
     },
   ],
